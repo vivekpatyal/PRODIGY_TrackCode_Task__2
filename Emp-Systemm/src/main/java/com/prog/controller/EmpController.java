48dd16a9125c8f4ea411d9180640dc234cae7b6c
vivek <vp3429782@gmail.com>
2024-06-29 14:10:16 +0530

    edit.html added sucessfully

--- Emp-Systemm/src/main/java/com/prog/controller/EmpController.java
+++ Emp-Systemm/src/main/java/com/prog/controller/EmpController.java
@@ -1,9 +1,13 @@
 package com.prog.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.prog.entity.Employee;
@@ -18,7 +22,11 @@ public class EmpController {
 	private EmpService service;
 	
 	@GetMapping("/")
-	public String home() {
+	public String home(Model m) {
+		
+		List<Employee> emp=service.getAllEmp();
+		m.addAttribute("emp",emp);
+		
 		return "index";
 	}
 	@GetMapping("/addemp")
@@ -35,5 +43,28 @@ public class EmpController {
 		return "redirect:/";
 		
 	}
-
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable int id,Model m)
+	{
+		
+		Employee e=service.getEmpById(id);
+		m.addAttribute("emp",e);
+		return "edit";
+		
+	}
+	@PostMapping("/update")
+	public String updateEmp(@ModelAttribute Employee e,HttpSession session)
+	{
+		service.addEmp(e);
+		session.setAttribute("msg", "Update Emloyee Sucessfully....");
+		return "redirect:/";
+	}
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable int id,HttpSession session)
+	{
+		service.deleteEmp(id);
+		session.setAttribute("msg", "Update Emloyee Sucessfully....");
+		return "redirect:/";
+		
+	}
 }
